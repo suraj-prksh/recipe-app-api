e404a47eb3b4964d0376b63c656326a34f39fb99
Suraj Prakash <prksh2182@gmail.com>
2021-07-24 20:08:23 +0530

    updated admin to support custom user model

--- app/core/admin.py
+++ app/core/admin.py
@@ -3,6 +3,7 @@ from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from core import models
 from django.utils.translation import gettext as _
 
+
 class UserAdmin(BaseUserAdmin):
     ordering = ['id']
     list_display = ['email', 'name']
@@ -13,7 +14,7 @@ class UserAdmin(BaseUserAdmin):
             _('Persmissions'),
             {'fields': ('is_active', 'is_staff', 'is_superuser')}
         ),
-        (_('Important dates'),{'fields': ('last_login',)})
+        (_('Important dates'), {'fields': ('last_login',)})
     )
     add_fieldsets = (
         (None, {
